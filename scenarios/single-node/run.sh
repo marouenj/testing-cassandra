628d38823aac6c0b776d36821a8307e1f2c39792
marouenj <marouen.jilani@gmail.com>
2016-01-20 09:53:03 +0900

    format script and add batch for cqlsh

--- scenarios/single-node/run.sh
+++ scenarios/single-node/run.sh
@@ -11,11 +11,16 @@ then
   docker rm single-node;
 fi
 
-docker run -d --name single-node -p 9042:9042 -v $(pwd)/single-node-config:/etc/cassandra cassandra
+docker run -d \
+       --name single-node \
+       -p 9042:9042 \
+       -v $(pwd)/single-node-config:/etc/cassandra \
+       -v $(pwd)/batch:/tmp/batch \
+       cassandra:latest
 
-docker exec -it single-node cqlsh
+docker exec -it single-node cqlsh -f /tmp/batch
 while [[ $(echo $?) = "1" ]];
 do
   sleep 1s
-  docker exec -it single-node cqlsh
+  docker exec -it single-node cqlsh -f /tmp/batch
 done
