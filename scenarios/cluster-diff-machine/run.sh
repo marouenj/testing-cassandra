e83608545b89e3caa6a8320faf55429408bd68fb
marouenj <marouen.jilani@gmail.com>
2016-01-21 14:52:56 +0900

    add scenario for intra-node cluster

---
+++ scenarios/cluster-diff-machine/run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+if [[ $1 == "stop" ]];
+then
+  for NODE in node1 node2;
+  do
+    docker rm -f ${NODE}
+  done
+fi
+
+if [[ $1 == "rm" ]];
+then
+  for NODE in node1 node2;
+  do
+    docker rm ${NODE}
+  done
+fi
+
+docker run -d \
+       --net weave \
+       --name node1 \
+       -h node1.weave.local \
+       $(weave dns-args) \
+       -v $(pwd)/batch:/tmp/batch \
+       cassandra:latest
+
+docker exec -it node1 cqlsh -f /tmp/batch
+while [[ $(echo $?) = "1" ]];
+do
+  sleep 2s
+  docker exec -it node1 cqlsh -f /tmp/batch
+done
+
+echo "GREP KEY CONFIG PARAMS..."
+docker exec -it node1 cat /etc/cassandra/cassandra.yaml | grep "\- seeds:"
+docker exec -it node1 cat /etc/cassandra/cassandra.yaml | grep "^cluster_name:"
+docker exec -it node1 cat /etc/cassandra/cassandra.yaml | grep "^listen_address:"
+docker exec -it node1 cat /etc/cassandra/cassandra.yaml | grep "^broadcast_address:"
+docker exec -it node1 cat /etc/cassandra/cassandra.yaml | grep "^rpc_address:"
+docker exec -it node1 cat /etc/cassandra/cassandra.yaml | grep "^broadcast_rpc_address:"
+
+
+for NODE in node2;
+do
+  docker run -d \
+         --net weave \
+         --name ${NODE} \
+         -h node2.weave.local \
+         $(weave dns-args) \
+         -v $(pwd)/batch:/tmp/batch \
+         -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.Networks.weave.IPAddress }}' node1)" \
+         cassandra:latest
+
+  docker exec -it ${NODE} cqlsh -f /tmp/batch
+  while [[ $(echo $?) = "1" ]];
+  do
+    sleep 2s
+    docker exec -it ${NODE} cqlsh -f /tmp/batch
+  done
+  
+  echo "GREP KEY CONFIG PARAMS..."
+  docker exec -it ${NODE} cat /etc/cassandra/cassandra.yaml | grep "\- seeds:"
+  docker exec -it ${NODE} cat /etc/cassandra/cassandra.yaml | grep "^cluster_name:"
+  docker exec -it ${NODE} cat /etc/cassandra/cassandra.yaml | grep "^listen_address:"
+  docker exec -it ${NODE} cat /etc/cassandra/cassandra.yaml | grep "^broadcast_address:"
+  docker exec -it ${NODE} cat /etc/cassandra/cassandra.yaml | grep "^rpc_address:"
+  docker exec -it ${NODE} cat /etc/cassandra/cassandra.yaml | grep "^broadcast_rpc_address:"
+done
